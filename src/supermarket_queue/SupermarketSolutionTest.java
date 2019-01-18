9dcb875200222fa40da21e0078627d96168902d9
VladmirVolgen <alvaroalonsoflor@gmail.com>
2019-01-18 12:01:43 +0000

    Add tests for supermarket

---
+++ src/supermarket_queue/SupermarketSolutionTest.java
@@ -0,0 +1,25 @@
+package supermarket_queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class SupermarketSolutionTest {
+
+	@Test
+	public void testNormalCondition() {
+		assertEquals(9, SupermarketSolution.solveSuperMarketQueue(new int[] { 2, 2, 3, 3, 4, 4 }, 2));
+	}
+
+	@Test
+	public void testEmptyArray() {
+		assertEquals(0, SupermarketSolution.solveSuperMarketQueue(new int[] {}, 1));
+	}
+
+	@Test
+	public void testSingleTillManyCustomers() {
+		assertEquals(15, SupermarketSolution.solveSuperMarketQueue(new int[] { 1, 2, 3, 4, 5 }, 1));
+	}
+
+}
