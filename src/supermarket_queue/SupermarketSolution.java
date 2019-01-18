bd08b2e19850fce1a798af0af2884c120fba89ac
VladmirVolgen <alvaroalonsoflor@gmail.com>
2019-01-18 12:30:36 +0000

    Create n tills in solveSupermarketSolution

--- src/supermarket_queue/SupermarketSolution.java
+++ src/supermarket_queue/SupermarketSolution.java
@@ -1,14 +1,25 @@
 package supermarket_queue;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SupermarketSolution {
 	
 	public static int solveSuperMarketQueue(int[] customers, int n) {
 		int result = 0;
 		
+		// create n tills
+		List<ArrayList<Integer>> tills = new ArrayList<>();
+		
+		for (int i = 0; i < n; i++) {
+			ArrayList<Integer> till = new ArrayList<>();
+			tills.add(till);
+		}
+		
+		
+		//add customers to tills
 		for (int customer : customers) {
-			result += customer;
+//			result += customer;
 		}
 		
 		return result;
